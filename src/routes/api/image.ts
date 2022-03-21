51429e0eeb70ee0b2d5b9a7f5d9269b9ad06a53e
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2022-03-22 01:37:34 +0200

    :sparkles: add fs functionality and install sharp with its types

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,13 +1,32 @@
 import { Router, Request, Response } from 'express';
-import { open } from 'fs/promises';
+import path from 'path';
+import fs from 'fs';
+import { validation } from '../../middlewares/validation';
 const routes = Router();
 
 // @desc      Review AND Resize Image
 // @route     GET /api/image/review/?filename
 // @route     GET /api/image/review/?filename&width&height
 // @access    Public
-routes.get('/', (req: Request, res: Response) => {
-  res.send('Welcome');
+routes.get('/', validation, async (req: Request, res: Response) => {
+  const { filename, width, height } = req.query;
+  const imagePath = path.join(
+    __dirname,
+    '../../assets/images',
+    `${filename}.jpg`
+  );
+  if (fs.existsSync(imagePath)) res.status(404).send('Image not found');
+  if (!width && !height) {
+    res.sendFile(imagePath);
+  }
+
+  const thumbImagePath = path.join(
+    __dirname,
+    '../../assets/thumbnails',
+    `${filename}_${width}_${height}.jpg`
+  );
+
+  if (fs.existsSync(thumbImagePath)) res.sendFile(thumbImagePath);
 });
 
 export default routes;
