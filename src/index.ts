c6b2b4e41c1105c73f066a7f5cdbc2ff035c24e3
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2022-03-22 02:24:01 +0200

    :boom: add sharp functionality

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import routes from './routes';
 const app = express();
 const port = 3000;
 
-app.use(routes);
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`..listening on port ${port}`);
--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,32 +1,39 @@
 import { Router, Request, Response } from 'express';
 import path from 'path';
 import fs from 'fs';
+import sharp from 'sharp';
 import { validation } from '../../middlewares/validation';
 const routes = Router();
 
 // @desc      Review AND Resize Image
-// @route     GET /api/image/review/?filename
-// @route     GET /api/image/review/?filename&width&height
+// @route     GET /api/image/?filename
+// @route     GET /api/image/?filename&width&height
 // @access    Public
 routes.get('/', validation, async (req: Request, res: Response) => {
   const { filename, width, height } = req.query;
   const imagePath = path.join(
     __dirname,
-    '../../assets/images',
+    '../../../assets/images',
     `${filename}.jpg`
   );
-  if (fs.existsSync(imagePath)) res.status(404).send('Image not found');
-  if (!width && !height) {
-    res.sendFile(imagePath);
-  }
+
+  if (!fs.existsSync(imagePath)) return res.status(404).send('Image not found');
+  if (!width && !height) return res.sendFile(imagePath);
 
   const thumbImagePath = path.join(
     __dirname,
-    '../../assets/thumbnails',
+    '../../../assets/thumbnails',
     `${filename}_${width}_${height}.jpg`
   );
 
-  if (fs.existsSync(thumbImagePath)) res.sendFile(thumbImagePath);
+  if (fs.existsSync(thumbImagePath)) return res.sendFile(thumbImagePath);
+
+  return await sharp(imagePath)
+    .resize(Number(width), Number(height))
+    .toFormat('jpg')
+    .toFile(thumbImagePath)
+    .then(() => res.sendFile(thumbImagePath))
+    .catch((err) => res.status(500).send(err));
 });
 
 export default routes;
